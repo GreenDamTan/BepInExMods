4b5d1ebac9f8fedf1307d2f153070db88b2d6050
GreenDamTan <GreenDamTan@users.noreply.github.com>
2023-08-29 14:22:52 +0800

    first commit

---
+++ SaintGearForcePlugin/Plugin.cs
@@ -0,0 +1,65 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using HarmonyLib.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SaintGearForcePlugin
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        static ManualLogSource gLog = new ManualLogSource("glog");
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            HarmonyFileLog.Enabled = true;
+            BepInEx.Logging.Logger.Sources.Add(gLog);
+            gLog.LogInfo("i am g glog");
+            Harmony _pluginTriggers = Harmony.CreateAndPatchAll(
+                typeof(Triggers)
+            );
+        }
+        private class Triggers
+        {
+            [HarmonyTranspiler]
+            [HarmonyPatch(typeof(PlayerStatusManeger), nameof(PlayerStatusManeger.Damage_HP))]
+            static IEnumerable<CodeInstruction> Damage_HP(IEnumerable<CodeInstruction> instructions)
+            {
+                var codes = new List<CodeInstruction>(instructions);
+                gLog.LogInfo("codes[63].opcode " + codes[63].opcode);
+                codes[63].opcode = System.Reflection.Emit.OpCodes.Ret;
+                return codes.AsEnumerable();
+            }
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(PlayerGrowUpSystem), "GetStatusGrowUpData")]
+            static public void GetStatusGrowUpData_Hook(ref StatusGrowUpData __result)
+            {
+                __result.consume_sp = 1;
+            }
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(PlayerGrowUpSystem), "GetGrowUpSkillData")]
+            static public void GetGrowUpSkillData_Hook(ref GrowUpSkillData __result)
+            {
+                __result.amount = 1;
+            }
+            [HarmonyPrefix]
+            [HarmonyPatch(typeof(PlayerStatusManeger), "Consume_EP")]
+            [HarmonyPatch(typeof(PlayerStatusManeger), "Consume_SP")]
+            static public void Consume_SP_EP(ref int consume_amount)
+            {
+                gLog.LogInfo("consume_amount" + consume_amount);
+                consume_amount = 1;
+            }
+            //[HarmonyPrefix]
+            //[HarmonyPatch(typeof(PlayerStatusManeger), nameof(PlayerStatusManeger.Damage_HP))]
+            //static public void Damage_HP_Hook(ref int damage_amount, ref bool acme)
+            //{
+            //    damage_amount = 1;
+            //}
+        }
+    }
+}
---
+++ Take_Me_To_The_Dungeon_Plugin/Plugin.cs
@@ -0,0 +1,65 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using HarmonyLib.Tools;
+using OHD.Card;
+using OHD.Dungeons.Player;
+using OHD.GameActions;
+using OHD.Players;
+using System;
+
+namespace Take_Me_To_The_Dungeon_Plugin;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BasePlugin
+{
+    static ManualLogSource gLog = new ManualLogSource("glog");
+    public override void Load()
+    {
+        // Plugin startup logic
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        HarmonyFileLog.Enabled = true;
+        BepInEx.Logging.Logger.Sources.Add(gLog);
+        gLog.LogInfo("i am g glog");
+        Harmony _pluginTriggers = Harmony.CreateAndPatchAll(
+                typeof(Triggers)
+            );
+    }
+    private class Triggers
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(OHD.Dungeons.Power.PowerBase), "get_Amount")]
+        static public void get_Amount_Hook(ref OHD.Dungeons.Power.PowerBase __instance, ref int __result)
+        {
+            if (__result == 0) { return; }
+            gLog.LogMessage("\n");
+            //var stackInfo = new System.Diagnostics.StackTrace(true);
+            //gLog.LogMessage(stackInfo.ToString());
+            gLog.LogMessage(__instance._Owner_k__BackingField.ToString());
+            gLog.LogMessage(__instance._PowerType_k__BackingField.ToString());
+            //gLog.LogMessage(__instance._ActiveDescription_k__BackingField.ToString());
+            gLog.LogMessage(__instance.ToString());
+            gLog.LogMessage(__result.ToString());
+            //foreach (var field in Traverse.Create(__instance).Properties())
+            //{
+            //    gLog.LogMessage(field.ToString());
+            //}
+            if (__instance._Owner_k__BackingField.ToString().Contains("BattleRoomPlayer"))
+            {
+                if (__instance._PowerType_k__BackingField.ToString().Contains("Debuff"))
+                {
+                    return;
+                }
+                __result = 999;
+            }
+        }
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(OHD.Card.CardBase), "get_CrystalCost")]
+        static public void get_CrystalCost_Hook(ref int __result)
+        {
+            //gLog.LogInfo("get_CrystalCost "+__result);
+            __result = 0;
+        }
+    }
+}
